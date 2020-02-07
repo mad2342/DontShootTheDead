db219521337669cfe0cb49b53f0840ed7771b40d
Mad <github@typ03.de>
2020-02-07 10:00:48 +0100

    bump version

--- Source/DontShootTheDead/DontShootTheDead.cs
+++ Source/DontShootTheDead/DontShootTheDead.cs
@@ -12,7 +12,7 @@ namespace DontShootTheDead
         internal static string ModDirectory;
 
         // BEN: DebugLevel (0: nothing, 1: error, 2: debug, 3: info)
-        internal static int DebugLevel = 3;
+        internal static int DebugLevel = 1;
 
         public static void Init(string directory, string settings)
         {
