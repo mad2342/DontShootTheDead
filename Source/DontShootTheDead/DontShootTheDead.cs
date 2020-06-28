29e783b76e0714be52062c33f0f2ed4ed0d23878
Mad <github@typ03.de>
2020-06-28 14:52:21 +0200

    fix resolve gen on kill

--- Source/DontShootTheDead/DontShootTheDead.cs
+++ Source/DontShootTheDead/DontShootTheDead.cs
@@ -12,6 +12,8 @@ namespace DontShootTheDead
         internal static string LogPath;
         internal static string ModDirectory;
 
+        internal static bool BuildSupportWeaponSequence = false;
+
         // BEN: DebugLevel (0: nothing, 1: error, 2: debug, 3: info)
         internal static int DebugLevel = 1;
 
@@ -27,47 +29,136 @@ namespace DontShootTheDead
         }
     }
 
-    // Melee
-    [HarmonyPatch(typeof(MechMeleeSequence), "FireWeapons")]
-    public static class MechMeleeSequence_FireWeapons_Patch
+
+
+    [HarmonyPatch(typeof(MechMeleeSequence), "ExecuteMelee")]
+    public static class MechMeleeSequence_ExecuteMelee_Patch
+    {
+        public static void Prefix(MechMeleeSequence __instance)
+        {
+            // Reset flag for weapon sequence
+            DontShootTheDead.BuildSupportWeaponSequence = false;
+        }
+    }
+
+    [HarmonyPatch(typeof(MechDFASequence), "ExecuteMelee")]
+    public static class MechDFASequence_ExecuteMelee_Patch
+    {
+        public static void Prefix(MechDFASequence __instance)
+        {
+            // Reset flag for weapon sequence
+            DontShootTheDead.BuildSupportWeaponSequence = false;
+        }
+    }
+
+
+
+    [HarmonyPatch(typeof(MechMeleeSequence), "BuildWeaponDirectorSequence")]
+    public static class MechMeleeSequence_BuildWeaponDirectorSequence_Patch
+    {
+        public static bool Prefix(MechMeleeSequence __instance, ref List<Weapon> ___requestedWeapons)
+        {
+            Logger.Info("[MechMeleeSequence_BuildWeaponDirectorSequence_PREFIX] DontShootTheDead.BuildSupportWeaponSequence: " + DontShootTheDead.BuildSupportWeaponSequence);
+
+            // Only build a weapon sequence if melee didn't already kill the target
+            if (DontShootTheDead.BuildSupportWeaponSequence)
+            {
+                // Will still only build a weapon sequence if ___requestedWeapons.Count is > 0
+                return true;
+            }
+            else
+            {
+                // This always needs to be done for a melee attack
+                ___requestedWeapons.RemoveAll((Weapon x) => x.Type == WeaponType.Melee);
+
+                return false;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(MechDFASequence), "BuildWeaponDirectorSequence")]
+    public static class MechDFASequence_BuildWeaponDirectorSequence_Patch
+    {
+        public static bool Prefix(MechDFASequence __instance, ref List<Weapon> ___requestedWeapons)
+        {
+            Logger.Info("[MechDFASequence_BuildWeaponDirectorSequence_PREFIX] DontShootTheDead.BuildSupportWeaponSequence: " + DontShootTheDead.BuildSupportWeaponSequence);
+
+            // Only build a weapon sequence if melee didn't already kill the target
+            if (DontShootTheDead.BuildSupportWeaponSequence)
+            {
+                // Will still only build a weapon sequence if ___requestedWeapons.Count is > 0
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+
+
+    [HarmonyPatch(typeof(MechMeleeSequence), "OnMeleeComplete")]
+    public static class MechMeleeSequence_OnMeleeComplete_Patch
     {
         public static void Prefix(MechMeleeSequence __instance, ref List<Weapon> ___requestedWeapons)
         {
-            // Skipping if no antipersonnel weapons are available
             if (___requestedWeapons.Count < 1)
             {
-                Logger.Info("[MechMeleeSequence_FireWeapons_PREFIX] No antipersonnel weapons available. Exit.");
                 return;
             }
 
             AbstractActor actor = __instance.owningActor;
-            //ICombatant MeleeTarget = (ICombatant)AccessTools.Property(typeof(MechMeleeSequence), "MeleeTarget").GetValue(__instance, null);
             ICombatant MeleeTarget = __instance.MeleeTarget;
 
             bool TargetIsFlaggedForDeath = MeleeTarget.IsFlaggedForDeath;
-            bool TargetIsDead = MeleeTarget.IsDead;
+            Logger.Info("[MechMeleeSequence_OnMeleeComplete_PREFIX] TargetIsFlaggedForDeath: " + TargetIsFlaggedForDeath);
 
-            Logger.Info("[MechMeleeSequence_FireWeapons_PREFIX] TargetIsFlaggedForDeath: " + TargetIsFlaggedForDeath);
-            Logger.Info("[MechMeleeSequence_FireWeapons_PREFIX] TargetIsDead: " + TargetIsDead);
-
-            if (TargetIsFlaggedForDeath || TargetIsDead)
+            if (TargetIsFlaggedForDeath)
             {
-                // As there is a function "Weapon.PreFireWeapon()" which substracts Ammo before the weapon actually fired we need to re-add it here!
-                foreach (Weapon w in ___requestedWeapons)
-                {
-                    w.IncrementAmmo();
-                }
-
-                // As the prepared weapon sequence will get interrupted, we need to apply resolve changes manually
-                int resolveValue = (MeleeTarget as AbstractActor).GetResolveValue();
-                Logger.Info($"[MechMeleeSequence_FireWeapons_PREFIX] resolveValue: {resolveValue}");
-                actor.team.ModifyMorale(resolveValue);
-
                 ___requestedWeapons.Clear();
                 actor.Combat.MessageCenter.PublishMessage(new FloatieMessage(actor.GUID, actor.GUID, "SUSPENDED SUPPORT WEAPONS", FloatieMessage.MessageNature.Neutral));
             }
+            else
+            {
+                DontShootTheDead.BuildSupportWeaponSequence = true;
+                Traverse BuildWeaponDirectorSequence = Traverse.Create(__instance).Method("BuildWeaponDirectorSequence");
+                BuildWeaponDirectorSequence.GetValue();
+            }
         }
     }
+
+    [HarmonyPatch(typeof(MechDFASequence), "OnMeleeComplete")]
+    public static class MechDFASequence_OnMeleeComplete_Patch
+    {
+        public static void Prefix(MechDFASequence __instance, ref List<Weapon> ___requestedWeapons)
+        {
+            if (___requestedWeapons.Count < 1)
+            {
+                return;
+            }
+
+            AbstractActor actor = __instance.owningActor;
+            ICombatant MeleeTarget = __instance.DFATarget;
+
+            bool TargetIsFlaggedForDeath = MeleeTarget.IsFlaggedForDeath;
+            Logger.Info("[MechDFASequence_OnMeleeComplete_PREFIX] TargetIsFlaggedForDeath: " + TargetIsFlaggedForDeath);
+
+            if (TargetIsFlaggedForDeath)
+            {
+                ___requestedWeapons.Clear();
+                actor.Combat.MessageCenter.PublishMessage(new FloatieMessage(actor.GUID, actor.GUID, "SUSPENDED SUPPORT WEAPONS", FloatieMessage.MessageNature.Neutral));
+            }
+            else
+            {
+                DontShootTheDead.BuildSupportWeaponSequence = true;
+                Traverse BuildWeaponDirectorSequence = Traverse.Create(__instance).Method("BuildWeaponDirectorSequence");
+                BuildWeaponDirectorSequence.GetValue();
+            }
+        }
+    }
+
+
 
     [HarmonyPatch(typeof(MechMeleeSequence), "DelayFireWeapons")]
     public static class MechMeleeSequence_DelayFireWeapons_Patch
@@ -76,56 +167,7 @@ namespace DontShootTheDead
         {
             // Was a hardcoded 5f
             timeout = 3f;
-            Logger.Debug("[MechMeleeSequence_DelayFireWeapons_PREFIX] SET Timeout: " + timeout);
-        }
-
-        public static void Postfix(MechMeleeSequence __instance, float timeout)
-        {
-            Logger.Debug("[MechMeleeSequence_DelayFireWeapons_POSTFIX] CHECK Timeout: " + timeout);
-        }
-    }
-
-
-
-    // DFA
-    [HarmonyPatch(typeof(MechDFASequence), "FireWeapons")]
-    public static class MechDFASequence_FireWeapons_Patch
-    {
-        public static void Prefix(MechDFASequence __instance, ref List<Weapon> ___requestedWeapons)
-        {
-            // Skipping if no antipersonnel weapons are available
-            if (___requestedWeapons.Count < 1)
-            {
-                Logger.Debug("[MechDFASequence_FireWeapons_PREFIX] No antipersonnel weapons available. Exit.");
-                return;
-            }
-
-            AbstractActor actor = __instance.owningActor;
-            //ICombatant DFATarget = (ICombatant)AccessTools.Property(typeof(MechDFASequence), "DFATarget").GetValue(__instance, null);
-            ICombatant DFATarget = __instance.DFATarget;
-
-            bool TargetIsFlaggedForDeath = DFATarget.IsFlaggedForDeath;
-            bool TargetIsDead = DFATarget.IsDead;
-
-            Logger.Info("[MechDFASequence_FireWeapons_PREFIX] TargetIsFlaggedForDeath: " + TargetIsFlaggedForDeath);
-            Logger.Info("[MechDFASequence_FireWeapons_PREFIX] TargetIsDead: " + TargetIsDead);
-
-            if (TargetIsFlaggedForDeath || TargetIsDead)
-            {
-                // As there is a function "Weapon.PreFireWeapon()" which substracts Ammo before the weapon actually fired we need to re-add it here!
-                foreach (Weapon w in ___requestedWeapons)
-                {
-                    w.IncrementAmmo();
-                }
-
-                // As the prepared weapon sequence will get interrupted, we need to apply resolve changes manually
-                int resolveValue = (DFATarget as AbstractActor).GetResolveValue();
-                Logger.Info($"[MechMeleeSequence_FireWeapons_PREFIX] resolveValue: {resolveValue}");
-                actor.team.ModifyMorale(resolveValue);
-
-                ___requestedWeapons.Clear();
-                actor.Combat.MessageCenter.PublishMessage(new FloatieMessage(actor.GUID, actor.GUID, "SUSPENDED SUPPORT WEAPONS", FloatieMessage.MessageNature.Neutral));
-            }
+            Logger.Debug("[MechMeleeSequence_DelayFireWeapons_PREFIX] timeout: " + timeout);
         }
     }
 
@@ -136,13 +178,7 @@ namespace DontShootTheDead
         {
             // Was a hardcoded 10f
             timeout = 6f;
-            Logger.Debug("[MechDFASequence_DelayFireWeapons_PREFIX] SET Timeout: " + timeout);
-        }
-
-        public static void Postfix(MechDFASequence __instance, float timeout)
-        {
-            Logger.Debug("[MechDFASequence_DelayFireWeapons_POSTFIX] CHECK Timeout: " + timeout);
+            Logger.Debug("[MechDFASequence_DelayFireWeapons_PREFIX] timeout: " + timeout);
         }
     }
 }
-
